eeba4ea10e556d9844a0fab33aff03fd5bc3b0a2
Noelia Donato <noeliadonato@gmail.com>
2022-06-12 18:11:11 -0300

    resolvimos case 1 de list search

--- list-search/case-1/src/App.tsx
+++ list-search/case-1/src/App.tsx
@@ -1,29 +1,57 @@
+import {useEffect, useState, useMemo} from "react";
 import type {Product} from "./types";
-
-import {useEffect, useState} from "react";
-
 import api from "./api";
+
+enum OrderType {
+  PRICE = 'price',
+  ALPHA = 'alphabetically',
+}
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
-  const [query, setQuery] = useState<string>("");
+  const [query, setQuery] = useState<string>(() => localStorage.getItem('query') || "");
+  const [order, setOrder] = useState<OrderType>(() => localStorage.getItem('order') as OrderType  || OrderType.PRICE);
 
   useEffect(() => {
-    api.search(query).then(setProducts);
+    api.search(query).then(data => setProducts(data));
+    localStorage.setItem('query', query);
   }, [query]);
+
+  useMemo(() => {
+    const orderedProducts = products.sort((a, b) => {
+      if (order === OrderType.PRICE) {
+        return a.price - b.price;
+      }
+      return a.title.localeCompare(b.title);
+    })
+
+    setProducts(orderedProducts);
+    localStorage.setItem('order', order);
+  },[order, products])
+
+  const handleSelect = (orderValue: string) => {
+     setOrder(orderValue as OrderType);
+  };
 
   return (
     <main>
       <h1>Tienda digitaloncy</h1>
-      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target.value)} />
+      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target.value)} value={query} />
+      <select onChange={(e) => handleSelect(e.target.value)} value={order}>
+        <option value={OrderType.PRICE}>Precio</option>
+        <option value={OrderType.ALPHA}>Alfabeticamente</option>
+      </select>
       <ul>
-        {products.map((product) => (
+        {products.length > 0 ?
+          products.map((product) => (
           <li key={product.id}>
             <h4>{product.title}</h4>
             <p>{product.description}</p>
-            <span>$ {product.price}</span>
+            <span>{new Intl.NumberFormat('es-AR', { style: 'currency', currency: 'ARS' }).format(product.price)}</span>
           </li>
-        ))}
+        ))
+        : <span>Cargando..</span>
+      }
       </ul>
     </main>
   );
