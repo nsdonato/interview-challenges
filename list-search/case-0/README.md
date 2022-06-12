bf65d9b24714a13d6ba7f65e1a850df68601c938
Noelia Donato <noeliadonato@gmail.com>
2022-06-12 18:10:52 -0300

    resolvimos case 0 de list search

--- list-search/case-0/README.md
+++ list-search/case-0/README.md
@@ -1,13 +1,13 @@
 # Ejercicios de entrevista
 
 ## Nivel 1
-- [ ] No se muestran los productos al cargar la aplicación.
+- [X] No se muestran los productos al cargar la aplicación.
 
 ## Nivel 2
-- [ ] Cuando buscamos no se muestran productos.
+- [X] Cuando buscamos no se muestran productos.
 
 ## Nivel 3
-- [ ] Los productos que salen 100 o menos deben tener la clase "sale" activada.
+- [X] Los productos que salen 100 o menos deben tener la clase "sale" activada.
 
 ## Extra
 - [ ] La aplicación tarda unos segundos hasta mostrar los productos sin mostrar un "cargando...".
--- list-search/case-0/src/App.tsx
+++ list-search/case-0/src/App.tsx
@@ -1,7 +1,5 @@
 import type {Product} from "./types";
-
 import {useEffect, useState} from "react";
-
 import api from "./api";
 
 function App() {
@@ -9,21 +7,22 @@ function App() {
   const [query, setQuery] = useState<string>("");
 
   useEffect(() => {
-    api.search(query);
+    api.search(query).then((data) => setProducts(data));
   }, [query]);
 
   return (
     <main>
       <h1>Tienda digitaloncy</h1>
-      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target)} />
+      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target.value)} />
       <ul>
-        {products.map((product) => (
-          <li key={product.id}>
+        {products.length > 0 ? products.map((product) => (
+          <li className={product.price <= 100 ? 'sale' : ''} key={product.id}>
             <h4>{product.title}</h4>
             <p>{product.description}</p>
             <span>$ {product.price}</span>
           </li>
-        ))}
+        ))
+      : <span>Cargando...</span>}
       </ul>
     </main>
   );
