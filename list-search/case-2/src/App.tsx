b4db2979e25b32ac287bd5b288df1e88654dd0e1
Noelia Donato <noeliadonato@gmail.com>
2022-06-12 21:01:42 -0300

    5hs de stream

--- list-search/case-2/src/App.tsx
+++ list-search/case-2/src/App.tsx
@@ -4,20 +4,24 @@ import {useEffect, useState} from "react";
 
 import api from "./api";
 
-function Recommended() {
+function Recommended () {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    api.search().then(setProducts);
+    api.search().then(data => {
+      const recommendedProducts = [...data]
+      .sort(() => (Math.random() > 0.5 ? 1 : -1))
+      .slice(0, 2)
+
+      setProducts(recommendedProducts)
+    });
   }, []);
 
   return (
     <main>
       <h1>Productos recomendados</h1>
       <ul>
-        {[...products]
-          .sort(() => (Math.random() > 0.5 ? 1 : -1))
-          .slice(0, 2)
+        {products
           .map((product) => (
             <li key={product.id}>
               <h4>{product.title}</h4>
@@ -30,21 +34,55 @@ function Recommended() {
   );
 }
 
-function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [query, setQuery] = useState<string>("");
+const useDebounce = (value: string, ms: number) => {
+  const [debounced, setDebounced] = useState(value);
 
   useEffect(() => {
-    api.search(query).then(setProducts);
-  }, [query]);
+    const timerId = setTimeout(() => {
+      setDebounced(value);
+    }, ms);
+
+    return () => {
+      clearTimeout(timerId);
+    }
+  }, [value, ms]);
+
+  return debounced
+}
+
+
+function App() {
+  const [products, setProducts] = useState<Product[]>(() => {
+    const storedValues = localStorage.getItem("products");
+    return storedValues ? JSON.parse(storedValues) : [];
+  });
+
+  const [query, setQuery] = useState<string>("");
+
+  const queryDebounced = useDebounce(query, 500);
+
+  useEffect(() => {
+    if(!products.length) {
+      api.search(queryDebounced).then(setProducts);
+    }
+  }, [queryDebounced]);
+
+  const handleChangeQuery = (valueQuery: string) => {
+    setQuery(valueQuery);
+  }
+  const handleAddToFavorite = (id: number) => {
+    const favProducts: Product[] = products.map((product: Product) => product.id === id ? {...product, favorite: !product.favorite} : product);
+    localStorage.setItem('products', JSON.stringify(favProducts));
+    setProducts(favProducts);
+  }
 
   return (
     <main>
       <h1>Tienda digitaloncy</h1>
-      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target.value)} />
+      <input name="text" placeholder="tv" type="text" onChange={(e) => handleChangeQuery(e.target.value)} />
       <ul>
         {products.map((product) => (
-          <li key={product.id}>
+          <li className={product.favorite ? 'fav' : ''} key={product.id} onClick={() => handleAddToFavorite(product.id)}>
             <h4>{product.title}</h4>
             <p>{product.description}</p>
             <span>$ {product.price}</span>
--- list-search/case-2/src/api.ts
+++ list-search/case-2/src/api.ts
@@ -6,30 +6,35 @@ const PRODUCTS: Product[] = [
     title: "TV 55 pulgadas",
     description: "TV 55 pulgadas, curva, marca gonzung",
     price: 1000,
+    favorite: false
   },
   {
     id: 2,
     title: "Webcam para streams",
     description: "Webcam marca logitency",
     price: 200,
+    favorite: false
   },
   {
     id: 3,
     title: "Celular gama alta",
     description: "Celular gama alta, marca xiaoncy",
     price: 300,
+    favorite: false
   },
   {
     id: 4,
     title: "Dron profesional",
     description: "Dron que soporta fuertes vientos, marca phantoncy",
     price: 500,
+    favorite: false
   },
   {
     id: 5,
     title: "Ventilador de escritorio",
     description: "Ventilador portable de escritorio, marca liliancy",
     price: 50,
+    favorite: false
   },
 ];
 
